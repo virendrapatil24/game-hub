323b31b82c9a0b266167c5b8093e9f3b98cb7fe0
Virendra Patil <virendrapatil.24vp@gmail.com>
2024-07-08 00:27:25 +0530

    added key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: "https://api.rawg.io/api",
     params: {
-        key: "XXX"
+        key: "fc53af6048394ae9bb8aad51170eec92"
     }
 })
